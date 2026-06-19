069ced9d2c9c76eb26cd8015a1fafe93f58c3a5f
Esteghlal1945848 <yyu466183@gmail.com>
2026-06-19 21:02:54 +0330

    fix circular import

--- database/models.py
+++ database/models.py
@@ -1,24 +1,43 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import (
+    Column,
+    Integer,
+    String
+)
+
 from database.core import Base
 
 
 class User(Base):
+
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True)
+    id = Column(
+        Integer,
+        primary_key=True
+    )
+
     telegram_id = Column(Integer)
+
     username = Column(String)
+
     full_name = Column(String)
 
 
 class File(Base):
+
     __tablename__ = "files"
 
-    id = Column(Integer, primary_key=True)
+    id = Column(
+        Integer,
+        primary_key=True
+    )
 
     file_id = Column(String)
+
     file_type = Column(String)
 
     grade = Column(String)
+
     major = Column(String)
+
     subject = Column(String)
