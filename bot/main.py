5a1f839023ae81c4f3f88ddb56d23e1d7b2070b1
Esteghlal1945848 <yyu466183@gmail.com>
2026-06-19 21:29:55 +0330

    remove old handlers

--- bot/main.py
+++ bot/main.py
@@ -1,40 +1,50 @@
 import asyncio
 
-from aiogram import Bot
-from aiogram import Dispatcher
+from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from aiogram.utils import executor
 
 from config import BOT_TOKEN
 
-from handlers import start
-from handlers.upload import handle_file
+from handlers.start import (
+    cmd_start,
+    handle_buttons
+)
+
+from handlers.upload import (
+    handle_file
+)
 
 
-bot = Bot(token=BOT_TOKEN)
-
-storage = MemoryStorage()
+bot = Bot(
+    token=BOT_TOKEN
+)
 
 dp = Dispatcher(
     bot,
-    storage=storage
+    storage=MemoryStorage()
 )
 
 
 async def main():
 
+    dp.message_handlers.handlers.clear()
+
     dp.register_message_handler(
-        start.cmd_start,
+        cmd_start,
         commands=["start"]
     )
 
     dp.register_message_handler(
-        start.handle_buttons
+        handle_buttons,
+        content_types=["text"]
     )
 
     dp.register_message_handler(
         handle_file,
-        content_types=["document", "video"]
+        content_types=[
+            "document",
+            "video"
+        ]
     )
 
     print("BOT STARTED")
