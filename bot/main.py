7128d82d2946f1f0513b0fab7c581afeff909f50
Esteghlal1945848 <yyu466183@gmail.com>
2026-06-25 22:29:04 +0330

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,19 +1,16 @@
 import asyncio
 import os
-import re
 
 from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.redis import RedisStorage2
 
-from database.core import get_db, init_db
-from database.models import Archive
+from database.core import init_db
 
 from handlers.start import cmd_start, handle_buttons, handle_file, handle_callback
-from aiogram.types import KeyboardButton
 
 
 ADMIN_ID = 7336595194
-CHANNEL_ID = -1003918140957
+CHANNEL_ID = -1002505515904
 
 
 async def on_startup(dp):
@@ -32,22 +29,15 @@ async def main():
     )
     dp = Dispatcher(bot, storage=storage)
 
-    # =====================
-    # ثبت هندلرها
-    # =====================
-
     dp.register_message_handler(cmd_start, commands=["start"])
-
     dp.register_message_handler(
         handle_file,
         content_types=[types.ContentType.DOCUMENT, types.ContentType.VIDEO]
     )
-
     dp.register_message_handler(
         handle_buttons,
         content_types=[types.ContentType.TEXT]
     )
-
     dp.register_callback_query_handler(handle_callback)
 
     await on_startup(dp)
