b7cefa1e924f28268c2421a2f578fbe19ab91578
Esteghlal1945848 <yyu466183@gmail.com>
2026-06-27 20:47:20 +0330

    Fix CHANNEL_LINK assignment in start.py

--- handlers/start.py
+++ handlers/start.py
@@ -14,7 +14,7 @@ from datetime import datetime
 
 ADMIN_ID = 7336595194
 CHANNEL_ID = -1002505515904
-CHANNEL_LINK "https://t.me/School_learny"
+CHANNEL_LINK = "https://t.me/School_learny"
 
 # ========================= helper =========================
 async def main_menu(user_id: int):
